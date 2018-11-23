2226c916dd85b089d49f19431c8cf19db224190b
Marius Casado <marius@viscaweb.com>
2018-11-23 12:17:20 +0100

    remove unused getters

--- src/main/java/algorithm/Person.java
+++ src/main/java/algorithm/Person.java
@@ -5,18 +5,5 @@ import java.util.Date;
 public class Person {
 	public String name;
 	public Date birthDate;
-
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public Date getBirthDate() {
-		return birthDate;
-	}
-	public void setBirthDate(Date birthDate) {
-		this.birthDate = birthDate;
-	}
 }
 
